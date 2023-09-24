19310fa56c1530a192cd90ee55181080d293afac
13jabbas <110024698+13jabbas@users.noreply.github.com>
2023-09-24 16:17:46 +0300

    Update 1main.py

--- 1main.py
+++ 1main.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-
 import seaborn as sns
 import plotly.graph_objects as go
 #Layout the page with headings 
