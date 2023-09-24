80a7ff4c092b54b3d17aeeac7ae82940b9de7315
13jabbas <110024698+13jabbas@users.noreply.github.com>
2023-09-24 15:24:47 +0300

    Update 1main.py

--- 1main.py
+++ 1main.py
@@ -2,7 +2,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
+
 import plotly.express as px
 from sklearn.ensemble import RandomForestRegressor
 import seaborn as sns
