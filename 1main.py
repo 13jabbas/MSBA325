8944b27dfa7061f7d1c675ecae046b84acd7cfcc
13jabbas <110024698+13jabbas@users.noreply.github.com>
2023-10-11 14:13:22 +0300

    Update 1main.py

--- 1main.py
+++ 1main.py
@@ -21,8 +21,8 @@ import pandas as pd
 file_path = os.path.join(os.path.dirname(__file__), 'Data', 'lego_sets_and_themes.csv')
 
 # Read the CSV file
-lego_data = pd.read_csv(file_path)
 
+lego_data = pd.read_csv('lego_sets_and_themes.csv')
 # Define Streamlit app
 with header:
     st.title('Lego Sales Across Time')
